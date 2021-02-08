fb1cc815283e0bca64a03748de6317d53839672d
Kym <kymmee922@protonmail.com>
2021-02-08 18:04:33 -0500

    Create dynamic Javascript navbar

---
+++ js/dynamicnavbar.js
@@ -0,0 +1,35 @@
+//dynamic menu for desktop
+
+const items = ['Home', 'Products', 'About', 'Testimonials'];
+
+for (item of items) {
+    const newItem = document.createElement('li'); //create new list node
+    const newLink = document.createElement('a'); // create new link node
+
+    const section_name = document.createTextNode(item); //grab each section name from the list 
+
+    newLink.appendChild(section_name); // append the section to a link
+    newLink.setAttribute('href', `#${item}`.toLowerCase()); // set the href
+    newLink.classList.add('nav-desktop'); // add the class
+    newItem.appendChild(newLink); // append link to new menu item
+
+    document.querySelector(".desktop").appendChild(newItem); // append UL with new list items
+};
+
+//dynamic menu for mobile
+
+const mobile_items = ['Home', 'Products', 'About', 'Testimonials', 'Contact'];
+
+for (item of mobile_items) {
+    const newItem = document.createElement('li'); //create new list node
+    const newLink = document.createElement('a'); // create new link node
+
+    const section_name = document.createTextNode(item); //grab each section name from the list 
+
+    newLink.appendChild(section_name); // append the section to a link
+    newLink.setAttribute('href', `#${item}`.toLowerCase()); // set the href
+    newLink.classList.add('nav-links'); // add the class
+    newItem.appendChild(newLink); // append link to new menu item
+
+    document.querySelector(".mobile_menu").appendChild(newItem); // append UL with new list items
+};
