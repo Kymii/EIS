b230b55ecdb3341baaab9838ce1be53387ef35ca
Kym <kymmee922@protonmail.com>
2021-02-08 15:19:48 -0500

    Remove unused code

--- js/activelinks.js
+++
@@ -1,8 +0,0 @@
-const links = document.querySelectorAll('.nav-desktop');
-
-for (const link of links) {
-    link.addEventListener('click', function() {
-        links.forEach((Link)=>Link.classList.remove('active'));
-        link.classList.add('active');
-    })
-}
